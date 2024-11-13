cee137ce524815831ce7bb0868247e5c952be57d
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2024-11-13 16:29:41 -0500

    deployment additions and cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
-
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,6 +8,8 @@ import productRoutes from "./routes/Product.router.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.get("/api/test", (req, res) => {
   res.status(200).send("Test...");
@@ -17,6 +19,14 @@ app.use(express.json()); // allows acceptance of json data in req.body
 
 // Pass session traffic to routes handler file
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.statis(path.join(__dirname, "/app/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "app", "dist", "index.html"))
+  );
+}
 
 app.listen(port, () => {
   connectDB(); // connect to db on server startup
